6cdadba3bfd07119535dd30e2c20e752c985f311
Geoff <gml57@georgetown.edu>
2018-10-17 14:27:00 -0400

    Parser for SGM datafiles
    Parser for SGM datafiles.  Still needs a bit of work.  Original work cited at top.

---
+++ Parser.py
@@ -0,0 +1,138 @@
+# Authors: Eustache Diemert <eustache@diemert.fr>
+#          @FedericoV <https://github.com/FedericoV/>
+# License: BSD 3 clause
+
+from __future__ import print_function
+from glob import glob
+import itertools
+import os.path
+import re
+
+
+import numpy as np
+
+
+from sklearn.externals.six.moves import html_parser
+from sklearn.datasets import get_data_home
+
+
+def _not_in_sphinx():
+    # Hack to detect whether we are running by the sphinx builder
+    return '__file__' in globals()
+
+###############################################################################
+# Reuters Dataset related routines
+# --------------------------------
+#
+
+
+class ReutersParser(html_parser.HTMLParser):
+    """Utility class to parse a SGML file and yield documents one at a time."""
+
+    def __init__(self, encoding='latin-1'):
+        html_parser.HTMLParser.__init__(self)
+        self._reset()
+        self.encoding = encoding
+
+    def handle_starttag(self, tag, attrs):
+        method = 'start_' + tag
+        getattr(self, method, lambda x: None)(attrs)
+
+    def handle_endtag(self, tag):
+        method = 'end_' + tag
+        getattr(self, method, lambda: None)()
+
+    def _reset(self):
+        self.in_title = 0
+        self.in_body = 0
+        self.in_topics = 0
+        self.in_topic_d = 0
+        self.title = ""
+        self.body = ""
+        self.topics = []
+        self.topic_d = ""
+
+    def parse(self, fd):
+        self.docs = []
+        for chunk in fd:
+            self.feed(chunk.decode(self.encoding))
+            for doc in self.docs:
+                yield doc
+            self.docs = []
+        self.close()
+
+    def handle_data(self, data):
+        if self.in_body:
+            self.body += data
+        elif self.in_title:
+            self.title += data
+        elif self.in_topic_d:
+            self.topic_d += data
+
+    def start_reuters(self, attributes):
+        pass
+
+    def end_reuters(self):
+        self.body = re.sub(r'\s+', r' ', self.body)
+        self.docs.append({'title': self.title,
+                          'body': self.body,
+                          'topics': self.topics})
+        self._reset()
+
+    def start_title(self, attributes):
+        self.in_title = 1
+
+    def end_title(self):
+        self.in_title = 0
+
+    def start_body(self, attributes):
+        self.in_body = 1
+
+    def end_body(self):
+        self.in_body = 0
+
+    def start_topics(self, attributes):
+        self.in_topics = 1
+
+    def end_topics(self):
+        self.in_topics = 0
+
+    def start_d(self, attributes):
+        self.in_topic_d = 1
+
+    def end_d(self):
+        self.in_topic_d = 0
+        self.topics.append(self.topic_d)
+        self.topic_d = ""
+
+
+def create_dataset(data_path=None):
+
+
+    parser = ReutersParser()
+    for filename in glob(os.path.join(data_path, "*.sgm")):
+        for doc in parser.parse(open(filename, 'rb')):
+            yield doc
+            
+            
+def get_batch(doc_iter, size, pos_class='*'):
+    
+    """Extract a minibatch of examples, return a tuple X_text, y.
+
+    Note: size is before excluding invalid docs with no topics assigned.
+
+    """
+    
+    data = [(u'{title}\n\n{body}'.format(**doc), pos_class in doc['topics'])
+            for doc in itertools.islice(doc_iter, size)
+            if doc['topics']]
+    if not len(data):
+        return np.asarray([], dtype=int), np.asarray([], dtype=int)
+    X_text, y = zip(*data)
+    return X_text, np.asarray(y, dtype=int)
+
+# Data path for my computer
+data_path = os.path.join(get_data_home(), "reuters")
+reuters_dataset = create_dataset(data_path)
+
+X_test_text, y_test = get_batch(reuters_dataset, 21000)
