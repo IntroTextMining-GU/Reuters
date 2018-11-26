9414b5f1bab0b0f6b366727357df4fc009517dc9
842974287 <842974287@qq.com>
2018-11-26 10:11:27 -0500

    data format modify

--- fastText/loadData.py
+++ fastText/loadData.py
@@ -1,7 +1,5 @@
 from nltk.corpus import reuters
 
-from sklearn.preprocessing import MultiLabelBinarizer
-
 def loadData():
     doc_id = reuters.fileids()
     train_id = [d for d in doc_id if d.startswith('training/')]
@@ -12,9 +10,5 @@ def loadData():
 
     train_label = [reuters.categories(doc_id) for doc_id in train_id]
     test_label = [reuters.categories(doc_id) for doc_id in test_id]
-
-    label_binarizer = MultiLabelBinarizer()
-    train_label = label_binarizer.fit_transform(train_label)
-    test_label = label_binarizer.transform(test_label)
 
     return {"train": train_data, "test": test_data}, {"train": train_label, "test": test_label}
